502712096394220874bc2d7f92d786902ca37cb7
laser-brain <laser-brain>
2022-01-03 11:38:07 +0100

    add recipe list view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import TheRecipeList from '@/views/TheRecipeList.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <TheRecipeList />
 </template>
 
 <style>
---
+++ src/views/TheRecipeList.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="recipes">
+  <div v-for="recipe in recipes" class="recipe-preview">
+    <img :src="recipe.thumbnail" alt="recipe" />
+    <h2>{{recipe.name}}</h2>
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+import { Ref, ref } from "vue";
+interface Recipe {
+  id: number,
+  name: string,
+  thumbnail: string
+}
+const recipes : Ref<Recipe[]> = ref([]);
+for (let index = 1; index <= 20; index++) {
+  recipes.value.push({
+    id: index,
+    name: `recipe ${index}`,
+    thumbnail: "https://www.themealdb.com/images/logo-small.png"
+  })  
+}
+</script>
+
+<style lang="scss">
+.recipes{
+  display: flex;
+  width: 100vw;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+  .recipe-preview {
+    margin: 1em;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+</style>
