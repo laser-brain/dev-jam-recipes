6eef4e0ce4df8a6bf4adc5884b95c9fb840eae7f
laser-brain <laser-brain>
2022-01-03 11:40:30 +0100

    cleanup default template styling

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import TheRecipeList from '@/views/TheRecipeList.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <TheRecipeList />
 </template>
 
@@ -14,12 +13,10 @@ import TheRecipeList from '@/views/TheRecipeList.vue'
   margin: 0;
   padding: 0;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
