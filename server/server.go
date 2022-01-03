a702ed6425f0b14524d2b989c4fb03494f69f54d
laser-brain <laser-brain>
2022-01-03 16:22:09 +0100

    added gin http server as entry point

--- server/server.go
+++ server/server.go
@@ -1,19 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"strconv"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+func getRecipes(c *gin.Context) {
+	pageParam := c.Query("page")
+	page, err := strconv.ParseInt(pageParam, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	sizeParam := c.Query("size")
+	size, err := strconv.ParseInt(sizeParam, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	skip := (page - 1) * size
+	recipesResponse := make(chan []Recipe)
+	go GetAllRecipesPaged(&skip, &size, recipesResponse)
+	recipes := <-recipesResponse
+	c.JSON(http.StatusOK, gin.H{"recipes": recipes})
+}
 
 func main() {
 	godotenv.Load()
 
-	page := 1
-	var size int64 = 2
-	var skip int64 = int64(page-1) * size
-	recipesResponse := make(chan []Recipe)
-	go GetAllRecipesPaged(&skip, &size, recipesResponse)
-	recipes := <-recipesResponse
-	fmt.Println(recipes)
+	router := gin.Default()
+	router.GET("/recipes", getRecipes)
+
+	router.Run("localhost:8080")
 }
